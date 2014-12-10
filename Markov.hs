c26d04b57f5979225df974d960c76bc6286379b0
Wilhelm Hedman <hedmanw@student.chalmers.se>
2014-12-10 15:15:18 +0100

    word lookup works!

--- Markov.hs
+++ Markov.hs
@@ -1,3 +1,4 @@
+import System.Random
 import Data.Ord
 import Data.List
 -- Markov generator
@@ -10,7 +11,7 @@ text :: String
 text = "of the people, by the people, for the people"
 
 wordList :: String -> [String]
-wordList str = words str
+wordList = words
 
 wordOrder2 :: [String] -> [(String,String)]
 wordOrder2 (x:[]) = []
@@ -18,3 +19,16 @@ wordOrder2 (f:x:xs) = (f,x) : wordOrder2 (x:xs)
 
 sortByWord :: [(String,String)] -> [(String,String)]
 sortByWord = sortBy (comparing fst)
+
+sortedList = sortByWord $ wordOrder2 $ wordList text
+
+getSuccessor :: [(String,String)] -> String -> StdGen -> String
+getSuccessor dict word stdgen = choose (filter (\w -> fst w == word) dict) stdgen
+  where choose :: [(String, String)] -> StdGen -> String
+        choose dict gen = snd (dict !! fst (randomR (0, length dict-1) gen))
+
+doEverything :: IO ()
+doEverything = do
+               gen <- newStdGen
+               putStrLn $ getSuccessor sortedList "the" gen
+
